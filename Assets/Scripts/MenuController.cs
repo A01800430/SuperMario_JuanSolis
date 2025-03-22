79958d581fd4f49e57363c1afd5181a1c12426f8
Juan Pablo Solis Gomez <a01800430@tec.mx>
2025-03-22 13:17:50 -0600

    MenuPrincipal

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MenuController : MonoBehaviour
+{
+    private UIDocument menu;
+    private Button Jugar;
+    private Button Ayuda;
+    private Button Creditos;
+
+    void OnEnable()
+    {
+        menu = GetComponent<UIDocument>();
+        var root = menu.rootVisualElement;
+        Jugar= root.Q<Button>("Jugar");
+        Ayuda = root.Q<Button>("Ayuda");
+        Creditos = root.Q<Button>("Creditos");
+
+
+        // Callbacks
+        Jugar.RegisterCallback<ClickEvent, String>(IniciarJuego, "SampleScene" );
+    }
+
+    private void IniciarJuego(ClickEvent evt, String escena)
+    {
+        SceneManager.LoadScene(escena);
+    }
+    
+}
